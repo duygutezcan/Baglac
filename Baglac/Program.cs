a9f9093d80f9aa4bd611837cd2d8f83f784fb451
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-03 08:53:47 +0300

    Add project files.

---
+++ Baglac/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Basla();
+
+void Basla()
+{
+    Console.WriteLine("Boyunuz kaç");
+    int boy = Convert.ToInt32(Console.ReadLine());
+    if (boy <= 150)
+    {
+        Console.WriteLine("kısa boylusunuz");
+    }
+    else if (boy > 150 && boy <=175)
+    {
+        Console.WriteLine("orta boylusunuz");
+    }
+    else if (boy > 175 && boy <= 210)
+    {
+        Console.WriteLine("uzun boylusunuz");
+    }
+    else 
+    {
+        Console.WriteLine("bir yanlışlık var");
+    }
+}
